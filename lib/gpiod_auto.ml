4f03ac496a7dec8832cdb177e8be22af3ea92a0c
Stefan Muenzel <source@s.muenzel.net>
2025-04-08 17:05:42 +0200

    WIP

--- lib/gpiod_auto.ml
+++ lib/gpiod_auto.ml
@@ -1,5 +1,83 @@
 external x : unit -> string array = "x"
 
+type gpiod_chip_info
+type gpiod_chip
+type gpiod_request_config
+type gpiod_line_config
+type gpiod_line_request
+type gpiod_line_info
+type gpiod_info_event
+
+type uint32 = int
+
+(*$
+open Cppcaml_gen
+
+external x : unit -> string array = "x"
+
+let () =
+  print_externals ()
+
+*)
+external gpiod_chip_open
+  : string -> gpiod_chip
+  = "ccwrap__gpiod_chip_open"
+
+external gpiod_chip_get_info
+  : gpiod_chip -> gpiod_chip_info
+  = "ccwrap__gpiod_chip_get_info"
+
+external gpiod_chip_get_path
+  : gpiod_chip -> string
+  = "ccwrap__gpiod_chip_get_path"
+
+external gpiod_chip_get_line_info
+  : gpiod_chip -> uint32 -> gpiod_line_info
+  = "ccwrap__gpiod_chip_get_line_info"
+
+external gpiod_chip_watch_line_info
+  : gpiod_chip -> uint32 -> gpiod_line_info
+  = "ccwrap__gpiod_chip_watch_line_info"
+
+external gpiod_chip_unwatch_line_info
+  : gpiod_chip -> uint32 -> int
+  = "ccwrap__gpiod_chip_unwatch_line_info"
+
+external gpiod_chip_get_fd
+  : gpiod_chip -> int
+  = "ccwrap__gpiod_chip_get_fd"
+
+external gpiod_chip_wait_info_event
+  : gpiod_chip -> int64 -> int
+  = "ccwrap__gpiod_chip_wait_info_event"
+
+external gpiod_chip_read_info_event
+  : gpiod_chip -> gpiod_info_event
+  = "ccwrap__gpiod_chip_read_info_event"
+
+external gpiod_chip_get_line_offset_from_name
+  : gpiod_chip -> string -> int
+  = "ccwrap__gpiod_chip_get_line_offset_from_name"
+
+external gpiod_chip_request_lines
+  : gpiod_chip -> gpiod_request_config -> gpiod_line_config -> gpiod_line_request
+  = "ccwrap__gpiod_chip_request_lines"
+
+external gpiod_chip_info_get_name
+  : gpiod_chip_info -> string
+  = "ccwrap__gpiod_chip_info_get_name"
+
+external gpiod_chip_info_get_label
+  : gpiod_chip_info -> string
+  = "ccwrap__gpiod_chip_info_get_label"
+
+external gpiod_chip_info_get_num_lines
+  : gpiod_chip_info -> int
+  = "ccwrap__gpiod_chip_info_get_num_lines"
+
+(* 14 functions *)
+(*$*)
+
 
 let%expect_test "" =
   Array.iter print_endline (x());
--- lib/gpiod_auto_stubs.cpp
+++ lib/gpiod_auto_stubs.cpp
@@ -36,5 +36,39 @@ template<>
 struct Cppcaml::CamlType<gpiod_chip_info*>
 : CamlTypeSharedPtrContainer<gpiod_chip_info, to_array("gpiod_chip_info"), FunDeleter<gpiod_chip_info_free>> { };
 
-DEF_CPPCAML(gpiod_chip_open, gpiod_chip_open, 1)
-DEF_CPPCAML(gpiod_chip_get_info, gpiod_chip_get_info, 1)
+template<>
+struct Cppcaml::CamlType<gpiod_line_info*>
+: CamlTypeSharedPtrContainer<gpiod_line_info, to_array("gpiod_line_info"), FunDeleter<gpiod_line_info_free>> { };
+
+template<>
+struct Cppcaml::CamlType<gpiod_info_event*>
+: CamlTypeSharedPtrContainer<gpiod_info_event, to_array("gpiod_info_event"), FunDeleter<gpiod_info_event_free>> { };
+
+template<>
+struct Cppcaml::CamlType<gpiod_request_config*>
+: CamlTypeSharedPtrContainer<gpiod_request_config, to_array("gpiod_request_config"), FunDeleter<gpiod_request_config_free>> { };
+
+template<>
+struct Cppcaml::CamlType<gpiod_line_config*>
+: CamlTypeSharedPtrContainer<gpiod_line_config, to_array("gpiod_line_config"), FunDeleter<gpiod_line_config_free>> { };
+
+template<>
+struct Cppcaml::CamlType<gpiod_line_request*>
+: CamlTypeSharedPtrContainer<gpiod_line_request, to_array("gpiod_line_request"), FunDeleter<gpiod_line_request_release>> { };
+
+DEF_CPPCAML_S(gpiod_chip_open, 1);
+DEF_CPPCAML_S(gpiod_chip_get_info, 1);
+DEF_CPPCAML_S(gpiod_chip_get_path, 1);
+DEF_CPPCAML_S(gpiod_chip_get_line_info, 2);
+DEF_CPPCAML_S(gpiod_chip_watch_line_info, 2);
+DEF_CPPCAML_S(gpiod_chip_unwatch_line_info, 2);
+DEF_CPPCAML_S(gpiod_chip_get_fd, 1);
+DEF_CPPCAML_S(gpiod_chip_wait_info_event, 2);
+// CR smuenzel: nullable
+DEF_CPPCAML_S(gpiod_chip_read_info_event, 1);
+DEF_CPPCAML_S(gpiod_chip_get_line_offset_from_name, 2);
+DEF_CPPCAML_S(gpiod_chip_request_lines, 3);
+DEF_CPPCAML_S(gpiod_chip_info_get_name, 1);
+DEF_CPPCAML_S(gpiod_chip_info_get_label, 1);
+DEF_CPPCAML_S(gpiod_chip_info_get_num_lines, 1);
+
